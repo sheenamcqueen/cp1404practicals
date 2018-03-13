e2eff13ddfb31066c221bf3b088711450ebee35b
sheenamcqueen <sheena.mcqueen@my.jcu.edu.au>
2018-03-13 10:31:22 +1000

    Add main and other functions

---
+++ prac_03/broken_score.py
@@ -0,0 +1,22 @@
+"""
+CP1404/CP5632 - Practical - Suggested Solution
+Fixed program to determine score status
+"""
+
+# Note boundary conditions (50 should be a pass, not > 50)
+# Note efficiency and nesting; use the fewest number of if/elif as possible
+def main():
+    score_evaluator()
+
+def score_evaluator():
+    score = float(input("Enter score: "))
+    if score < 0 or score > 100:
+        print("Invalid score")
+    elif score >= 90:
+        print("Excellent")
+    elif score >= 50:
+        print("Passable")
+    else:
+        print("Bad")
+
+main()
