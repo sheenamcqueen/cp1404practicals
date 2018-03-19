6ca9bbd8a3eb561d0f1766542695485c1e50c594
sheenamcqueen <sheena.mcqueen@my.jcu.edu.au>
2018-03-20 09:13:44 +1000

    Initial commit

---
+++ prac_04/list_tests.py
@@ -0,0 +1,20 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+print(numbers[0])
+print(numbers[-1])
+print(numbers[3])
+print(numbers[:-1])
+print(numbers[3:4])
+print(5 in numbers)
+print(7 in numbers)
+print("3" in numbers)
+print(numbers + [6, 5, 3])
+
+numbers[1] = "ten"
+print(numbers[1])
+
+numbers[-1] = 1
+print(numbers[-1])
+
+print(numbers[1:])
+
+print(9 in numbers)
