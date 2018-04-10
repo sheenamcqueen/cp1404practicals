c671d0d3f026f86d4342b45be005e1eb71b133c2
Sheena <sheena.mcqueen@my.jcu.edu.au>
2018-04-10 21:05:59 +1000

    Completes all but the guitars prac task

--- prac_07/guitars_test.py
+++ prac_07/guitars_test.py
@@ -1,10 +1,18 @@
 
-from prac_07.guitars import Guitar
+from prac_07.guitar import Guitar
 
 
 def main():
     gibson = Guitar("Gibson L-5 CES", 1922, 16035.40)
-    print (gibson)
+    another_guitar = Guitar("Another Guitar", 2012, 0.00)
+    guitars = [gibson, another_guitar]
 
+    for guitar in guitars:
+        print(guitar)
+
+    print("{} get_age() - Expected {}. Got {}".format(gibson.name, 96, gibson.get_age()))
+    print("{} get_age() - Expected {}. Got {}".format(another_guitar.name, 6, another_guitar.get_age()))
+    print("{} is_vintage() - Expected {}. Got {}".format(gibson.name, True, gibson.is_vintage()))
+    print("{} is_vintage() - Expected {}. Got {}".format(another_guitar.name, False, another_guitar.is_vintage()))
 
 main()
