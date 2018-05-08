301e5f18b7969fcc1052f535433daccfcf6dac64
jc451099 <sheena.mcqueen@my.jcu.edu.au>
2018-05-08 10:23:26 +1000

    completes all work for practical 8 and begins work on practical 9

--- prac_08/taxi_simulator.py
+++ prac_08/taxi_simulator.py
@@ -6,27 +6,40 @@ from prac_08.silver_service_taxi import SilverServiceTaxi
 def main():
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
     print("Let's drive!")
-    menu_selection = input("q)uit, c)hoose taxi, d)rive")
-    chosen_taxi = 0
+    chosen_taxi = taxis[0]
+    total_cost = 0
+    print("Current taxi: {}".format(chosen_taxi.name))
+    menu_selection = input("q)uit, c)hoose taxi, d)rive").lower()
     while menu_selection != "q":
         if menu_selection == "c":
-            choose_taxi(taxis)
+            chosen_taxi = taxis[choose_taxi(taxis)]
         elif menu_selection == "d":
-            drive_taxi(chosen_taxi)
-        menu_selection = input("q)uit, c)hoose taxi, d)rive")
-    print("Total trip cost: ${:.2f}")
+            total_cost += drive_taxi(chosen_taxi)
+        print("Bill to date: ${:.2f}".format(total_cost))
+        print("Current taxi: {}".format(chosen_taxi.name))
+        menu_selection = input("q)uit, c)hoose taxi, d)rive").lower()
+    print("Total trip cost: ${:.2f}\nTaxis are now:".format(total_cost))
+    count = 0
+    for i, taxi in enumerate(taxis):
+        print("{} - {}".format(count, taxi))
+        count += 1
+
+
 
 def choose_taxi(taxis):
     count = 0
-    for taxi in taxis:
+    for i, taxi in enumerate(taxis):
         print("{} - {}".format(count, taxi))
         count += 1
-    chosen_taxi = input(taxis[])
+    chosen_taxi = int(input())
     return chosen_taxi
 
 def drive_taxi(chosen_taxi):
-    distance = input("Drive how far?")
+    chosen_taxi.start_fare()
+    distance = int(input("Drive how far?"))
     chosen_taxi.drive(distance)
-    print ("Bill to date: ${:.2f}".format(chosen_taxi.get_fare()))
+    cost = chosen_taxi.get_fare()
+    print("Your {} trip cost you ${:.2f}".format(chosen_taxi.name, cost))
+    return cost
 
 main()
